939e16b40494112347389062388f9bd76815197f
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2024-06-10 10:50:54 +0200

    expand vector2 functionality

--- src/vector2.rs
+++ src/vector2.rs
@@ -4,8 +4,31 @@ use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
 pub struct Vector2<T>(pub T, pub T);
 
 impl Vector2<f32> {
+    pub const ZERO: Vector2<f32> = Vector2(0.0, 0.0);
+    pub const UP: Vector2<f32> = Vector2(0.0, 1.0);
+    pub const DOWN: Vector2<f32> = Vector2(0.0, -1.0);
+    pub const LEFT: Vector2<f32> = Vector2(-1.0, 0.0);
+    pub const RIGHT: Vector2<f32> = Vector2(1.0, 0.0);
+
     pub fn dot(&self, rhs: &Self) -> f32 {
         self.0 * rhs.0 + self.1 * rhs.1
+    }
+
+    pub fn len(&self) -> f32 {
+        (self.0 * self.0 + self.1 * self.1).sqrt()
+    }
+
+    pub fn normalise(&mut self) {
+        *self /= self.len();
+    }
+
+    pub fn normalised(&self) -> Self {
+        self.clone() / self.len()
+    }
+
+    pub fn to_normalised(mut self) -> Self {
+        self /= self.len();
+        self
     }
 }
 
@@ -66,5 +89,33 @@ impl DivAssign<Vector2<f32>> for Vector2<f32> {
     fn div_assign(&mut self, rhs: Vector2<f32>) {
         self.0 /= rhs.0;
         self.1 /= rhs.1;
+    }
+}
+
+impl AddAssign<f32> for Vector2<f32> {
+    fn add_assign(&mut self, rhs: f32) {
+        self.0 += rhs;
+        self.1 += rhs;
+    }
+}
+
+impl SubAssign<f32> for Vector2<f32> {
+    fn sub_assign(&mut self, rhs: f32) {
+        self.0 -= rhs;
+        self.1 -= rhs;
+    }
+}
+
+impl MulAssign<f32> for Vector2<f32> {
+    fn mul_assign(&mut self, rhs: f32) {
+        self.0 *= rhs;
+        self.1 *= rhs;
+    }
+}
+
+impl DivAssign<f32> for Vector2<f32> {
+    fn div_assign(&mut self, rhs: f32) {
+        self.0 /= rhs;
+        self.1 /= rhs;
     }
 }
