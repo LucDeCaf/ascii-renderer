182749d79aeacd58415927a7089589724791acdd
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2024-06-11 07:47:12 +0200

    fix viewport being treated as smallen in draw calls + add bbox checks for optimisation

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ use ascii_renderer::vector2::Vector2;
 
 trait Drawable {
     fn point_in_self(&self, point: &Vector2<f32>) -> bool;
+    fn bbox(&self) -> Rect;
 }
 
 #[derive(Debug, Clone)]
@@ -31,6 +32,10 @@ impl Drawable for Rect {
 
         (self.position.0..max_x).contains(&point.0) && (self.position.1..max_y).contains(&point.1)
     }
+
+    fn bbox(&self) -> Rect {
+        self.clone()
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -46,6 +51,14 @@ impl Drawable for Circle {
         let distance = ((x_diff * x_diff) + (y_diff * y_diff)).sqrt();
 
         distance <= self.radius
+    }
+
+    fn bbox(&self) -> Rect {
+        Rect {
+            width: self.radius * 2.0,
+            height: self.radius * 2.0,
+            position: self.position.clone(),
+        }
     }
 }
 
@@ -74,14 +87,31 @@ impl<'a> Renderer<'a> {
 
     fn bbox(&self) -> Rect {
         Rect {
-            position: self.position.clone()
-                - Vector2(
-                    (self.options.viewport_width / 2) as f32,
-                    (self.options.viewport_height / 2) as f32,
-                ),
+            position: self.position.clone(),
+            // - Vector2(
+            //     (self.options.viewport_width / 2) as f32,
+            //     (self.options.viewport_height / 2) as f32,
+            // ),
             width: self.options.viewport_width as f32,
             height: self.options.viewport_height as f32,
         }
+    }
+
+    fn collides_with_rect(&self, rect: &Rect) -> bool {
+        let self_left = self.position.0;
+        let self_right = self_left + self.options.viewport_width as f32;
+        let self_top = self.position.1;
+        let self_bottom = self_top + self.options.viewport_height as f32;
+
+        let rect_left = rect.position.0;
+        let rect_right = rect_left + rect.width;
+        let rect_top = rect.position.1;
+        let rect_bottom = rect_top + rect.height;
+
+        self_left < rect_right
+            && rect_left < self_right
+            && rect_top > self_bottom
+            && self_top > rect_bottom
     }
 
     fn walk(&mut self, direction: Vector2<f32>, distance: f32) {
@@ -157,6 +187,15 @@ impl<'a> Renderer<'a> {
         // Clear buffer
         self.buffer.fill(' ');
 
+        // Only check shapes where bbox collides with camera
+        let mut shapes_to_check = vec![];
+        for shape in self.drawables.iter() {
+            let bbox = shape.bbox();
+            if self.collides_with_rect(&bbox) {
+                shapes_to_check.push(*shape);
+            }
+        }
+
         // Render content
         for point in self.local_pixels() {
             for shape in self.drawables.iter() {
@@ -197,7 +236,7 @@ impl<'a> Renderer<'a> {
         execute!(stdout, MoveTo(0, 0))?;
 
         for line in self.lines() {
-            let mut out = String::with_capacity(self.options.viewport_width);
+            let mut out = String::with_capacity(self.options.viewport_width * 2);
 
             for c in line.chars() {
                 if out.len() + 2 > out.capacity() {
@@ -218,9 +257,12 @@ fn main() -> std::io::Result<()> {
     let size = size()?;
 
     let mut renderer = Renderer::new(RendererOptions {
-        viewport_width: size.0 as usize,
+        viewport_width: (size.0 / 2) as usize,
         viewport_height: size.1 as usize,
     });
+
+    let bbox = renderer.bbox();
+    renderer.add_drawable(&bbox);
 
     let circle = Circle {
         radius: 10.0,
